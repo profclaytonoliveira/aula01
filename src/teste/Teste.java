8f4e4c9a48bfeef86ada4fd27cbeb90e15408d8a
Clayton Oliveira <csoltom@gmail.com>
2020-11-04 22:49:25 -0300

    MEnsagem de boa noite

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -5,6 +5,8 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Teste feito");
 		System.out.println("Teste feito");
+		System.out.println("Boa noite");
+		
 	}
 
 }
